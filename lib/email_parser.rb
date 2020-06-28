a55e44d8185810c2716a9af22b88c12163b22717
Amana Ammishaddai <amana.ammi@gmail.com>
2020-06-28 00:57:16 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -17,7 +17,7 @@ class EmailAddressParser
   def parse(csv_data)
     #need to use .uniq
     @email_address.split.collect do |data|
-      data.split(",")
+      data.split(',')
     end
     .flatten.uniq
   end
