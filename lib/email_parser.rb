51d7f36ede22f9f87ea88c94511c77c49acffbcf
Amana Ammishaddai <amana.ammi@gmail.com>
2020-06-28 00:48:12 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,8 +3,7 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 
-require 'pry
-'
+require 'pry'
 class EmailAddressParser
   
   attr_accessor :email_addresses, :name
